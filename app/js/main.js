86bf6fa7d4e3797f5cb561e725fa3635d9f67648
BigHeadliner <bigheadliner@gmail.com>
2022-10-02 21:22:28 +0300

    tour section ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,34 @@
-$(function () {   
-     
-    
- 
-
-  
- 
-  
- 
-  
-  
-  
-  
-     
-
- 
+$(function () {
 
 
+    const counterUp = window.counterUp.default
 
-  
+    const callback = entries => {
+        entries.forEach(entry => {
+            const el = entry.target
+            if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                counterUp(el, {
+                    duration: 2000,
+                    delay: 16,
+                })
+                el.classList.add('is-visible')
+            }
+        })
+    }
+
+    const IO1 = new IntersectionObserver(callback, { threshold: 1 })
+    const IO2 = new IntersectionObserver(callback, { threshold: 1 })
+    const IO3 = new IntersectionObserver(callback, { threshold: 1 })
+
+    const el1 = document.querySelector('.tour__statistics-num1')
+    const el2 = document.querySelector('.tour__statistics-num2')
+    const el3 = document.querySelector('.tour__statistics-num3')
 
 
-  
-  
-   
+    IO1.observe(el1)
+    IO2.observe(el2)
+    IO3.observe(el3)
+
+
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,8 @@ function nunjucks(){
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js', 
     'node_modules/rateyo/src/jquery.rateyo.js', 
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js', 
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js', 
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',  
+    'node_modules/counterup2/dist/index.js', 
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',   
     'app/js/main.js'
    ]) 
